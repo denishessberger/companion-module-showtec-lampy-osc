5f055938711c1fc29d4279531ee16f59bd9b7667
Denis Hessberger <denis@denis-hessberger.de>
2023-12-22 11:44:42 +0100

    Added Virtual GrandMaster, Virtual Chase Speed Fader, Virtual FX Size Fader

--- index.js
+++ index.js
@@ -25,27 +25,27 @@ class LampyInstance extends InstanceBase {
 
   getConfigFields() {
     return [{
-        type: 'static-text',
-        id: 'info',
-        width: 12,
-        label: 'Information',
-        value: 'To enable OSC on Lampy, enable OSC in the Menu, Show Settings, OSC.',
-      },
-      {
-        type: 'textinput',
-        id: 'host',
-        label: 'Target IP',
-        tooltip: 'The IP of the Lampy console',
-        width: 6,
-        regex: Regex.IP,
-      },
-      {
-        type: 'textinput',
-        id: 'port',
-        label: 'Target Port',
-        width: 4,
-        regex: Regex.PORT,
-      },
+      type: 'static-text',
+      id: 'info',
+      width: 12,
+      label: 'Information',
+      value: 'To enable OSC on Lampy, enable OSC in the Menu, Show Settings, OSC.',
+    },
+    {
+      type: 'textinput',
+      id: 'host',
+      label: 'Target IP',
+      tooltip: 'The IP of the Lampy console',
+      width: 6,
+      regex: Regex.IP,
+    },
+    {
+      type: 'textinput',
+      id: 'port',
+      label: 'Target Port',
+      width: 4,
+      regex: Regex.PORT,
+    },
     ]
   }
 
@@ -64,23 +64,27 @@ class LampyInstance extends InstanceBase {
       }
     }
 
+    /************************
+    * PLAYBACK FADER COMMANDS
+    ************************/
+
     this.setActionDefinitions({
       pbFader: {
         name: 'Playback Fader Value',
         options: [{
-            type: 'textinput',
-            label: 'Playback fader (1-10)',
-            id: 'pbId',
-            default: '1',
-            regex: Regex.NUMBER,
-          },
-          {
-            type: 'textinput',
-            label: 'Fader value (0-1000)',
-            id: 'pbVal',
-            default: '',
-            regex: Regex.NUMBER,
-          },
+          type: 'textinput',
+          label: 'Playback fader (1-10)',
+          id: 'pbId',
+          default: '1',
+          regex: Regex.NUMBER,
+        },
+        {
+          type: 'textinput',
+          label: 'Fader value (0-1000)',
+          id: 'pbVal',
+          default: '',
+          regex: Regex.NUMBER,
+        },
         ],
         callback: (action) => {
           var arg = {
@@ -91,30 +95,29 @@ class LampyInstance extends InstanceBase {
           sendOSC('/lampy/pbf/' + action.options.pbId + '/value', arg)
         },
       },
-
       pbButtonPress: {
         name: 'Playback Button',
         options: [{
-            type: 'textinput',
-            label: 'Playback (1-10)',
-            id: 'pbId',
-            default: '1',
-            regex: Regex.NUMBER,
+          type: 'textinput',
+          label: 'Playback (1-10)',
+          id: 'pbId',
+          default: '1',
+          regex: Regex.NUMBER,
+        },
+        {
+          type: 'dropdown',
+          label: 'Push / Release',
+          id: 'btnAction',
+          choices: [{
+            id: '1.',
+            label: 'Push'
           },
           {
-            type: 'dropdown',
-            label: 'Push / Release',
-            id: 'btnAction',
-            choices: [{
-                id: '1.',
-                label: 'Push'
-              },
-              {
-                id: '0.',
-                label: 'Release'
-              },
-            ],
-          }
+            id: '0.',
+            label: 'Release'
+          },
+          ],
+        }
 
         ],
         callback: (action) => {
@@ -126,6 +129,10 @@ class LampyInstance extends InstanceBase {
           sendOSC('/lampy/pbf/' + action.options.pbId + '/flash', arg)
         },
       },
+
+      /************************
+      * PLAYBACK PAGE COMMANDS
+      ************************/
 
       pbPageNextButton: {
         name: 'Playback Page +',
@@ -139,47 +146,51 @@ class LampyInstance extends InstanceBase {
           sendOSC('/lampy/pbf/page/next', arg)
         },
       },
-			pbPagePreviousButton: {
-				name: 'Playback Page -',
-				options: [
-				],
-				callback: (action) => {
-					var arg = {
-						type: 'f',
-						value: 1,
-					}
-					sendOSC('/lampy/pbf/page/previous', arg)
-				},
-			},
-			pbPageTemplateButton: {
-				name: 'Template Page',
-				options: [
-				],
-				callback: (action) => {
-					var arg = {
-						type: 'f',
-						value: 1,
-					}
-					sendOSC('/lampy/pbf/page/template', arg)
-				},
-			},
+      pbPagePreviousButton: {
+        name: 'Playback Page -',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/pbf/page/previous', arg)
+        },
+      },
+      pbPageTemplateButton: {
+        name: 'Template Page',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/pbf/page/template', arg)
+        },
+      },
+
+      /************************
+      * MULTI FUNCTION FADER COMMANDS
+      ************************/
 
       mffFader: {
         name: 'MFF Value',
         options: [{
-            type: 'textinput',
-            label: 'MFF (1-30)',
-            id: 'mffId',
-            default: '1',
-            regex: Regex.NUMBER,
-          },
-          {
-            type: 'textinput',
-            label: 'Fader value (0-1000)',
-            id: 'mffVal',
-            default: '',
-            regex: Regex.NUMBER,
-          },
+          type: 'textinput',
+          label: 'MFF (1-30)',
+          id: 'mffId',
+          default: '1',
+          regex: Regex.NUMBER,
+        },
+        {
+          type: 'textinput',
+          label: 'Fader value (0-1000)',
+          id: 'mffVal',
+          default: '',
+          regex: Regex.NUMBER,
+        },
         ],
         callback: (action) => {
           var arg = {
@@ -194,26 +205,26 @@ class LampyInstance extends InstanceBase {
       mffButtonPress: {
         name: 'MFF Button',
         options: [{
-            type: 'textinput',
-            label: 'MFF (1-30)',
-            id: 'mffId',
-            default: '1',
-            regex: Regex.NUMBER,
+          type: 'textinput',
+          label: 'MFF (1-30)',
+          id: 'mffId',
+          default: '1',
+          regex: Regex.NUMBER,
+        },
+        {
+          type: 'dropdown',
+          label: 'Push / Release',
+          id: 'btnAction',
+          choices: [{
+            id: '1.',
+            label: 'Push'
           },
           {
-            type: 'dropdown',
-            label: 'Push / Release',
-            id: 'btnAction',
-            choices: [{
-                id: '1.',
-                label: 'Push'
-              },
-              {
-                id: '0.',
-                label: 'Release'
-              },
-            ],
-          }
+            id: '0.',
+            label: 'Release'
+          },
+          ],
+        }
         ],
         callback: (action) => {
           var arg = {
@@ -224,7 +235,12 @@ class LampyInstance extends InstanceBase {
           sendOSC('/lampy/mff/' + action.options.mffId + '/flash', arg)
         },
       },
-			mffModeButton: {
+
+      /************************
+      * MFF MODE COMMANDS
+      ************************/
+
+      mffModeButton: {
         name: 'MFF Mode Button',
         options: [
         ],
@@ -236,65 +252,70 @@ class LampyInstance extends InstanceBase {
           sendOSC('/lampy/mff/mode/mode_button', arg)
         },
       },
-			mffModeFixtureButton: {
-				name: 'MFF Mode Fixture',
-				options: [
-				],
-				callback: (action) => {
-					var arg = {
-						type: 'f',
-						value: 1,
-					}
-					sendOSC('/lampy/mff/mode/fixture', arg)
-				},
-			},
-			mffModeGroupButton: {
-				name: 'MFF Mode Group',
-				options: [
-				],
-				callback: (action) => {
-					var arg = {
-						type: 'f',
-						value: 1,
-					}
-					sendOSC('/lampy/mff/mode/group', arg)
-				},
-			},
-			mffModeSceneButton: {
-				name: 'MFF Mode Scene',
-				options: [
-				],
-				callback: (action) => {
-					var arg = {
-						type: 'f',
-						value: 1,
-					}
-					sendOSC('/lampy/mff/mode/scene', arg)
-				},
-			},
+      mffModeFixtureButton: {
+        name: 'MFF Mode Fixture',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/mff/mode/fixture', arg)
+        },
+      },
+      mffModeGroupButton: {
+        name: 'MFF Mode Group',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/mff/mode/group', arg)
+        },
+      },
+      mffModeSceneButton: {
+        name: 'MFF Mode Scene',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/mff/mode/scene', arg)
+        },
+      },
+
+      /************************
+      * VIRTUAL EXECUTOR BUTTONS
+      ************************/
+
       execButtonPress: {
         name: 'Executor Button',
         options: [{
-            type: 'textinput',
-            label: 'Executor (1-40)',
-            id: 'execId',
-            default: '1',
-            regex: Regex.NUMBER,
+          type: 'textinput',
+          label: 'Executor (1-40)',
+          id: 'execId',
+          default: '1',
+          regex: Regex.NUMBER,
+        },
+        {
+          type: 'dropdown',
+          label: 'Push / Release',
+          id: 'btnAction',
+          choices: [{
+            id: '1.',
+            label: 'Push'
           },
           {
-            type: 'dropdown',
-            label: 'Push / Release',
-            id: 'btnAction',
-            choices: [{
-                id: '1.',
-                label: 'Push'
-              },
-              {
-                id: '0.',
-                label: 'Release'
-              },
-            ],
-          }
+            id: '0.',
+            label: 'Release'
+          },
+          ],
+        }
         ],
         callback: (action) => {
           var arg = {
@@ -304,6 +325,214 @@ class LampyInstance extends InstanceBase {
           sendOSC('/lampy/virtual_executor/' + action.options.execId + '/flash', arg)
         },
       },
+
+      /************************
+       * VIRTUAL GRAND MASTER FADER COMMANDS
+       ************************/
+
+      gmFader: {
+        name: 'Grand Master Value',
+        options: [
+          {
+            type: 'textinput',
+            label: 'Fader value (0-1000)',
+            id: 'gmVal',
+            default: '',
+            regex: Regex.NUMBER,
+          },
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'i',
+            value: action.options.gmVal,
+          }
+
+          sendOSC('/lampy/virtual_fader/grand_master/value', arg)
+        },
+      },
+      gmResetButton: {
+        name: 'GrandMaster Reset Button',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/virtual_fader/grand_master/reset', arg)
+        },
+      },
+      gmResetButton: {
+        name: 'GrandMaster Blackout Button',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/virtual_fader/grand_master/blackout', arg)
+        },
+      },
+
+      /************************
+      * VIRTUAL CHASE SPEED FADER COMMANDS
+      ************************/
+
+      chaseSpdFader: {
+        name: 'Chase Speed Value',
+        options: [
+          {
+            type: 'textinput',
+            label: 'Fader value (0-1000)',
+            id: 'chSpdVal',
+            default: '',
+            regex: Regex.NUMBER,
+          },
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'i',
+            value: action.options.chSpdVal,
+          }
+
+          sendOSC('/lampy/virtual_fader/chase_speed/value', arg)
+        },
+      },
+      chaseSpdResetButton: {
+        name: 'Chase Speed Reset Button',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/virtual_fader/chase_speed/reset', arg)
+        },
+      },
+      chaseSpdDoubleButton: {
+        name: 'Chase Speed Double Button',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/virtual_fader/chase_speed/double', arg)
+        },
+      },
+      chaseSpdHalfButton: {
+        name: 'Chase Speed Half Button',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/virtual_fader/chase_speed/half', arg)
+        },
+      },
+      chaseSpdPauseButton: {
+        name: 'Chase Speed Pause Button',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/virtual_fader/chase_speed/pause', arg)
+        },
+      },
+      chaseSpdTapButton: {
+        name: 'Chase Speed Tap Button',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/virtual_fader/chase_speed/tap_speed', arg)
+        },
+      },
+
+      /************************
+      * VIRTUAL FX SIZE FADER COMMANDS
+      ************************/
+
+      fxSizeFader: {
+        name: 'FX Size Value',
+        options: [
+          {
+            type: 'textinput',
+            label: 'Fader value (0-1000)',
+            id: 'fxSizeVal',
+            default: '',
+            regex: Regex.NUMBER,
+          },
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'i',
+            value: action.options.fxSizeVal,
+          }
+
+          sendOSC('/lampy/virtual_fader/global_fx_size/value', arg)
+        },
+      },
+      fxSizeResetButton: {
+        name: 'FX Size Reset Button',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/virtual_fader/global_fx_size/reset', arg)
+        },
+      },
+      fxSizeDoubleButton: {
+        name: 'FX Size Double Button',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/virtual_fader/global_fx_size/double', arg)
+        },
+      },
+      fxSizeHalfButton: {
+        name: 'FX Size Half Button',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/virtual_fader/global_fx_size/half', arg)
+        },
+      },
+      fxSizePauseButton: {
+        name: 'FX Size Zero Button',
+        options: [
+        ],
+        callback: (action) => {
+          var arg = {
+            type: 'f',
+            value: 1,
+          }
+          sendOSC('/lampy/virtual_fader/global_fx_size/zero', arg)
+        },
+      },
+
     })
   }
 }
